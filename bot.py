2f7057519c29aec0d46af18bf8edec3cf0b50932
Leo Balduf <leobalduf@gmail.com>
2019-04-23 13:43:15 +0300

    Harden and clean up EXIF stuff

--- bot.py
+++ bot.py
@@ -45,20 +45,48 @@ def document_msg_handler(bot, update):
         size_y = str(im.size[1])
         logger.debug("Got image size = {},{}".format(size_x,size_y))
         print(f.name)
+        reply = "Original resolution: {}x{}".format(size_x,size_y)
 	
-        # EXIF data!
-        ff = open(f.name,'rb')
-        tags = exifread.process_file(ff)
-        logger.debug("Got EXIF data = {}".format(tags))
-        manufacturer = tags["Image Make"]
-        model = tags["Image Model"]
-        iso = tags["EXIF ISOSpeedRatings"]
-        aperture = float(Fraction(str(tags["EXIF FNumber"])))
-        exposure = tags["EXIF ExposureTime"]
-        focal_len = round(float(Fraction(str(tags["EXIF FocalLength"]))))
+        try: 
+            # EXIF data!
+            with open(f.name,'rb') as ff:
+                tags = exifread.process_file(ff)
+                logger.debug("Got EXIF data = {}".format(tags))
+                if tags:
+                    manufacturer = tags.get("Image Make","???")
+                    model = tags.get("Image Model","???")
+                    iso = tags.get("EXIF ISOSpeedRatings","???")
+                    
+                    aperture_raw = tags.get("EXIF FNumber","")
+                    if aperture_raw:
+                        aperture = float(Fraction(str(aperture_raw)))
+                    else:
+                        aperture = "???"
+                    
+                    exposure_raw = tags.get("EXIF ExposureTime","")
+                    if exposure_raw:
+                        frac = Fraction(str(exposure_raw))
+                        if frac.denominator > 1000:
+                            exposure = str(round(frac,3))
+                        else:
+                            exposure = str(frac)
+                    else:
+                        exposure = "???"
+                    
+                    focal_len_raw = tags.get("EXIF FocalLength","")
+                    if focal_len_raw:
+                        focal_len = round(float(Fraction(str(focal_len_raw))))
+                    else:
+                        focal_len = "???"
 
-        picmsg = msg.reply_photo(f,
-                                 "Original resolution: {}x{}; Model: {} {}; Focal Length: {} mm; ISO {}, f/{}, {} s".format(size_x,size_y,manufacturer, model, focal_len, iso, aperture, exposure))
+                    reply += "; Model: {} {}; f: {}mm; ISO {}, f/{}, {}s".format(manufacturer, model, focal_len, iso, aperture, exposure)
+                else:
+                    reply += "; no EXIF data :("
+        except:
+            reply += "; error processing EXIF data :("
+
+        picmsg = msg.reply_photo(f,reply)
+
     print(msg.document)
 
 
